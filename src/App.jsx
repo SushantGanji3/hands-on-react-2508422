458003df56bf01e132929889d898005a15baaba0
SushantGanji3 <69330961+SushantGanji3@users.noreply.github.com>
2024-06-05 19:20:32 +0000

    Finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+//below two examples are the same exact effect hooks are simplier tho
+//classes or components kinda like functions
+/*class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h1>Meet the {this.props.name}</h1>
+  }
+}*/
+
+
+//hooks
+const Welcome = (props) => {
+  return (
+    <h1>Meet the <i>{props.name}</i></h1>
+  )
+}
+
 function App() {
   const name = 'StarGazers'
   return (
@@ -5,7 +27,9 @@ function App() {
       <article>
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
-          <h1>Meet the <i style={{ color: "SteelBlue" }}>{name}</i></h1>
+          <Welcome name="Stars"/>
+          <Welcome name="StarGazers"/>
+          <Welcome name="Staraliens"/>
           <p>Members of an <b>intergalactic alliance</b><br />
             paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
           <button className="outline" onClick={() => alert('Hi there')}>Click Me</button>
